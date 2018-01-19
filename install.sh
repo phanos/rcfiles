6ebc8dd3274a2fa730d30d74ec8b80c54a8d00de
Pete <petephan@gmail.com>
2018-01-19 14:14:48 -0800

    script for symlinking to $HOME

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ls $HOME
+for f in .vimrc .screenrc; do
+	if [ -e $HOME/$f ]; then
+		echo skipping $f, already exists
+	else
+		echo linking $f
+		ln -s $HOME/rcfiles/$f $HOME/$f
+	fi
+done
